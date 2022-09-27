a07c0c2c6209b330227a5800c0af373bb6263466
renniequin@gmail.com <renniequin@gmail.com>
2022-09-27 16:21:18 +0300

    creates variables that hold values associated with the fields

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apikey = document.querySelector('.api-key');
+
+//results
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
+
+
