5c362908d59551ca9492460eee542d61bc3dfa45
renniequin@gmail.com <renniequin@gmail.com>
2022-09-28 21:36:07 +0300

    fixes some typos on reading api value

--- src/index.js
+++ src/index.js
@@ -12,54 +12,88 @@ const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
 
-
-form.addEventListener('submit', (e)=> handleSubmit(e));
-clearBtn.addEventListener('click', (e)=> reset(e));
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
 init();
 
 function init() {
-    //if anything is in the local storage, pick it up
-    const storedApiKey = localStorage.getItem('apiKey');
-    const storedRegion = localStorage.getItem('regionName');
+	//if anything is in localStorage, pick it up
+	const storedApiKey = localStorage.getItem('apiKey');
+	const storedRegion = localStorage.getItem('regionName');
 
-    //set icon to be generic green
-    //todo
+	//set icon to be generic green
+	//todo
 
-    if(storedApiKey === null || storedRegion === null) {
-        //if we dont have the keys show the form
-        form.style.display = 'block';
-        results.style.display = 'none';
+	if (storedApiKey === null || storedRegion === null) {
+		//if we don't have the keys, show the form
+		form.style.display = 'block';
+		results.style.display = 'none';
 		loading.style.display = 'none';
 		clearBtn.style.display = 'none';
 		errors.textContent = '';
-    }else {
-        //if jeys/regions in localStorage, show results when they load
+	} else {
+        //if we have saved keys/regions in localStorage, show results when they load
         displayCarbonUsage(storedApiKey, storedRegion);
-            results.style.display = 'none';
-            form.style.display = 'none';
-            clearBtn.style.display = 'block'
-    }
+		results.style.display = 'none';
+		form.style.display = 'none';
+		clearBtn.style.display = 'block';
+	}
 };
 
 function reset(e) {
-    e.preventDefault();
-    //clear local storage for region only
-    localStorage.removeItem('regionName');
-    init();
+	e.preventDefault();
+	//clear local storage for region only
+	localStorage.removeItem('regionName');
+	init();
 }
 
 function handleSubmit(e) {
     e.preventDefault(); //stop browser from refreshing
-    setUpUser(apikey.ariaValueMax, region.value);
+    setUpUser(apikey.value, region.value);
 }
 
-function setUpUser (apiKey, regionName){
-    localStorage.setItem('apiKey', apiKey);
-    localStorage.setItem('regionName', regionName);
+function setUpUser(apiKey, regionName) {
+	localStorage.setItem('apiKey', apiKey);
+	localStorage.setItem('regionName', regionName);
+	loading.style.display = 'block';
+	errors.textContent = '';
+	clearBtn.style.display = 'block';
+	//make initial call
+	displayCarbonUsage(apiKey, regionName);
+}
 
-    loading.style.display = 'block'
-    errors.textContent = '';
-    clearBtn.style.display = 'block';
+ import axios from '../node_modules/axios';
 
-    displayCarbonUsage(apiKey,regionName);
+async function displayCarbonUsage(apiKey, region) {
+	try {
+		await axios
+			.get('https://api.co2signal.com/v1/latest', {
+				params: {
+					countryCode: region,
+				},
+				headers: {
+					'auth-token': apiKey,
+				},
+			})
+			.then((response) => {
+				let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+				//calculateColor(CO2);
+
+				loading.style.display = 'none';
+				form.style.display = 'none';
+				myregion.textContent = region;
+				usage.textContent =
+					Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+				fossilfuel.textContent =
+					response.data.data.fossilFuelPercentage.toFixed(2) +
+					'% (percentage of fossil fuels used to generate electricity)';
+				results.style.display = 'block';
+			});
+	} catch (error) {
+		console.log(error);
+		loading.style.display = 'none';
+		results.style.display = 'none';
+		errors.textContent = 'Sorry, we have no data for the region you have requested.';
+	}
 }
